2bc3038c20f5e534c808ddd64a1d69643edfd5a5
Guilherme_Pereira <guilherme098321@gmail.com>
2025-11-13 02:32:46 -0300

    alteração da porta do servidor

--- app.py
+++ app.py
@@ -234,3 +234,8 @@ if __name__ == '__main__':
 
     def __repr__(self):
         return f'<Cliente {self.nome}>'
+    
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
